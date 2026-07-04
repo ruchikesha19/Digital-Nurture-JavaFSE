d07c8c6492494079a7de5c3c67a8954c5905ec8a
ruchi <ruchikesha1903@gmail.com>
2026-07-04 17:18:24 +0530

    Exercise-5-SpringIoC

---
+++ Week-2/SpringCoreMaven/Exercise-5-SpringIoC/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void getBooks() {
+        System.out.println("BookRepository: Retrieving catalog of books from IoC Container...");
+    }
+}
