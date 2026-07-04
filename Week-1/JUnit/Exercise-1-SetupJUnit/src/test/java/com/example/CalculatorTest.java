9bc06993232347503c607fefb39b517334730e1b
ruchi <ruchikesha1903@gmail.com>
2026-07-04 13:50:20 +0530

    Exercise-1-SetupJUnit

---
+++ Week-1/JUnit/Exercise-1-SetupJUnit/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+}
