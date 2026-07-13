5f6ab5e932e2ae3c03799a12c65e4821d1a46296
ruchi <ruchikesha1903@gmail.com>
2026-07-13 09:25:51 +0530

    Week-4: Edge Services and API Gateway with Spring Boot 3 and Spring Cloud exercises

---
+++ Week-4/Microservices/Edge Services and API Gateway with Spring  Boot 3 and Spring Cloud/Exercise-1-Routing-Filtering/src/test/java/com/cognizant/gateway/GatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cognizant.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
