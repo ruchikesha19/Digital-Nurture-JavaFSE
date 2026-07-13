6f37ccc4eab5003eaa5299f0dd4734ec246dde97
ruchi <ruchikesha1903@gmail.com>
2026-07-13 09:28:52 +0530

    Week-4: Microservices with API gateway exercises

---
+++ Week-4/Microservices/Microservices with API gateway/Exercise-2-API-Gateway-LogFilter/greet-service/src/main/java/com/cts/greet/GreetApplication.java
@@ -0,0 +1,11 @@
+package com.cts.greet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetApplication.class, args);
+    }
+}
