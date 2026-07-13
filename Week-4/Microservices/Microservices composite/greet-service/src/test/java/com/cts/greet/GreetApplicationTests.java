a4ded8388e387ad43c7d391fb9d50f1efa5c794b
ruchi <ruchikesha1903@gmail.com>
2026-07-13 09:27:40 +0530

    Week-4: Microservices composite exercises

---
+++ Week-4/Microservices/Microservices composite/greet-service/src/test/java/com/cts/greet/GreetApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cts.greet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
